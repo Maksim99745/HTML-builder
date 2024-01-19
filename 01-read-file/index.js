5158019c106c2de8fe24e8dd0fbe44ac7680cab0
Maksim99745 <maksimmuhametzyanov11@gmail.com>
2024-01-19 14:39:47 +0100

    fix: add read stream to 01 task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,14 +1,26 @@
 const path = require('path');
 const fs = require('fs');
+const { error } = require('console');
 
-const filePath = path.resolve(__dirname, 'text.txt');
 
-const fileText = fs.readFile(filePath, {encoding: 'utf-8'}, (err, fileData) => {
-  if (err) {
+async function readFile () {
+  try {
+    const filePath = path.join(__dirname, 'text.txt');
+    
+    const readStream = fs.createReadStream(filePath, {encoding: 'utf-8'});
+
+    readStream.on('data', (chunk) => {
+      console.log(chunk);
+    })
+
+    readStream.on('error', (error) => {
+      console.log(error);
+    })
+
+  } catch (err) {
     console.log(err);
-    return;
-  } else {
-    console.log(fileData);
+  
   }
-})
+}
+readFile();
 
